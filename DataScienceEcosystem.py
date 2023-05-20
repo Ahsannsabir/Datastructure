c06be0c380791320d9a8382b4a0cd833521dc82a
Ahsannsabir <131555366+Ahsannsabir@users.noreply.github.com>
2023-05-20 16:39:53 +0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives**
+# - List popular languages for Data Science
+# - Effectively use Jupyter Notebook
+# - Understand R language
+
+# ## Author
+# Ahsan Sabir
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. Julia
+# 3. R
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. Seaborn
+# 
+
+# |Data science Tools|
+# -----------------------------
+# |Jupyter Notebook|
+# |GitHub|
+# |R Studio|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+print((3*4)+5)
+
+
+# In[4]:
+
+
+print(200/60)
+
+
+# In[ ]:
+
+
+
+
